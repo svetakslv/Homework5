e2e19debad27abcf08f3b44fb9ddbdabffd2df07
Светка <sveta.chakrygina@mail.ru>
2022-08-27 21:25:38 +0300

    Задачи №1 и 2

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача №1, 2, 3
+        int[] num = new int [3];
+        int a = 1;
+        num[0] = a;
+        int b = 2;
+        num[1] = b;
+        int c = 3;
+        num[2] = c;
+        System.out.println(num[0] + ", ");
+        System.out.println(num[1] + ", ");
+        System.out.println(num[2]);
+
+        double[] frac = new double[]{1.57, 7.654, 9.986};
+        double d = 1.57;
+        frac[0] = d ;
+        double e = 7.654;
+        frac[1] = e;
+        double f = 9.986;
+        frac[2] = f;
+        System.out.println(frac[0] + ", ");
+        System.out.println(frac[1] + ", ");
+        System.out.println(frac[2]);
+
+        int[] i = {4, 7, 2, 9};
+        int g = 4;
+        i[0] = g;
+        int h = 7;
+        i[1] = h;
+        int k = 2;
+        i[2] = k;
+        int l = 9;
+        i[3] = l;
+        System.out.println(i[0] + ", ");
+        System.out.println(i[1] + ", ");
+        System.out.println(i[2] + ", ");
+        System.out.println(i[3]);
+
+
+
+    }
+}
